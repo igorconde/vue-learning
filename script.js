ffb176f58476d78c42b970187d5ddfedac57454b
igorconde <igorgabrielconde@gmail.com>
2020-11-27 00:47:06 -0300

    Ciclo de vida dos componentes

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const bandeira = (pais) =>
-  `<img src="/paises/${pais}.png" style="width:30px" />`;
-
-Vue.component("pais", {
-  props: ["bandeira", "continente"],
-  template: `<p v-html="bandeira + continente"></p>`,
-});
+/*
+beforeCreate - Antes da execução (Geralmente usado para influenciar em outros elementos)
+created - Quando o elemento é criado
+beforeMount - Quando o elemento foi criado porém não é exibido na tela
+mounted = quando o elemento é renderizado na tela
+beforeUpdate - Antes de atualização
+updated: quando o elemento é atualizado ou modificado
+beforeDestroy - Antes do elemento destruid
+destroyed - quando o elemento é destruido
+*/
 
 let app = new Vue({
   el: "#app",
   data: {
-    alemanha: bandeira("alemanha"),
-    brasil: bandeira("brasil"),
-    china: bandeira("china"),
-    portugal: bandeira("portugal"),
-    usa: bandeira("usa"),
+    nome: "Igor",
+  },
+  beforeCreate: function () {
+    console.log(this.nome);
+  },
+  created: function () {
+    console.log(this.nome);
+  },
+  beforeMount: function () {
+    console.log(this.nome);
+  },
+  mounted: function () {
+    console.log(this.nome);
+  },
+  beforeUpdate: function () {
+    console.log(this.nome);
+  },
+  updated: function () {
+    console.log(this.nome);
+  },
+  beforeDestroy: function () {
+    console.log(this.nome);
+  },
+  destroyed: function () {
+    console.log(this.nome);
   },
 });
