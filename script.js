d12b0c30f594b93b6d53a1b7c7f3fe036763b656
igorconde <igorgabrielconde@gmail.com>
2020-11-28 12:17:47 -0300

    get and setters

--- script.js
+++ script.js
@@ -17,8 +17,15 @@ let app = new Vue({
   },
   methods: {},
   computed: {
-    nomeCompleto: function () {
-      return `${this.primeiroNome} ${this.segundoNome}`;
+    nomeCompleto: {
+      get: function () {
+        return `${this.primeiroNome} ${this.segundoNome}`;
+      },
+      set: function (novoValor) {
+        let nomes = novoValor.split(" ");
+        this.primeiroNome = nomes[0];
+        this.segundoNome = nomes[1];
+      },
     },
   },
 });
