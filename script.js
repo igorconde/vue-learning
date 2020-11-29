2e63ec35db9ac516a1d163dc23646cd9c264b83f
igorconde <igorgabrielconde@gmail.com>
2020-11-29 01:05:06 -0300

    componente ação simples

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-Vue.component("menuTest", {
+Vue.component("contador", {
   data: function () {
     return {
-      texto: "Texto Qualquer",
+      c: 0,
     };
   },
-  template: `<button>{{texto}}</button>`,
+  methods: {
+    increment: function () {
+      this.c++;
+    },
+  },
+  template: `
+          <div>
+          <span class="text-base font-medium text-gray-500 hover:text-gray-900">{{c}}X 
+          <a href="#" @click="increment" class="ml-2 whitespace-nowrap inline-flex items-center justify-center px-4 py-0 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            Aumentar
+          </a>
+          </span>
+          </div>`,
 });
 
 let app = new Vue({
