a3f8474b18e1e18e89eb6a63c3b1ed0515b7ef66
igorconde <igorgabrielconde@gmail.com>
2020-11-29 01:21:19 -0300

    Props with components

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-//Global Component
-Vue.component("contador", {
-  templae: `<button>Clicou 3x</button>`,
-});
-
-//Local Component
-
-let menuSuperior = {
-  template: `<span>Menu</span>`,
+let postagem = {
+  props: ["titulo", "corpo"],
+  template: `<div><h2>{{titulo}}<br><span>{{corpo}}</span></h2></div>`,
 };
 
-let main = new Vue({
+let app = new Vue({
   el: "#main",
-  data: {
-    title: "Teste",
-  },
+  data: {},
   components: {
-    "menu-superior": menuSuperior,
-  },
-  computed: {},
-  watch: {},
-  methods: {},
-});
-
-//2 Stancia
-
-let footer = new Vue({
-  el: "#footer",
-  data: {
-    footer: "Teste",
+    postagem,
   },
   computed: {},
   watch: {},
